64342a5affffd08890cae1c9e1b6bfeba15a1c68
Hasan Ozovali <ozovalihasan@gmail.com>
2020-12-01 17:28:37 +0300

    Add book model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_080539) do
+ActiveRecord::Schema.define(version: 2020_12_01_135808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "category"
+    t.string "complete_percentage"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -22,4 +33,5 @@ ActiveRecord::Schema.define(version: 2020_12_01_080539) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "books", "users"
 end
